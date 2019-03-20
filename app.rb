eaf4c178cc8c240ace0c52a5d203963ee99b324b
Wardah <wlt_93@hotmail.com>
2019-03-20 16:46:17 +0000

    Passes test after POST/redirect/GET pattern

--- app.rb
+++ app.rb
@@ -1,20 +1,27 @@
 require 'sinatra/base'
 
 class Battle < Sinatra::Base
+  enable :sessions
 
   get "/" do
     erb(:index)
   end   
 
+  get "/play" do
+    @player_1_name = session[:player_1_name] 
+    @player_2_name = session[:player_2_name]
+    erb(:play) 
+  end 
+
+
   post "/names" do
-    @player_1_name = params[:player_1_name]
-    @player_2_name = params[:player_2_name]
-    erb :play
+    session[:player_1_name]= params[:player_1_name]
+    session[:player_2_name] = params[:player_2_name]
+    redirect '/play'
   end   
 
-
-# start the server if ruby file executed directly
-run! if app_file == $0
+  # start the server if ruby file executed directly
+  run! if app_file == $0
 
 end
 
--- spec/features/form_name_spec.rb
+++ spec/features/form_name_spec.rb
@@ -5,16 +5,5 @@ feature 'Name form' do
     fill_in(:player_2_name, with: 'Mittens')
     click_button 'Submit'
     expect(page).to have_content "Dave vs. Mittens"
-    #finding an input tab called name in the form and checks if it has a value
-    # find_field('name').value
   end
-
-  # scenario 'expects players to fill in their names in a form' do
-  #   visit('/')
-  #   fill_in('name', with: 'Viking')
-  #   visit('/names')
-  #   expect(page).to have_content('Viking')
-
-  # end
-
 end
