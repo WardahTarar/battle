d363d092cebda84134a1b1a18b5e2d72b66041b9
Wardah <wlt_93@hotmail.com>
2019-03-20 16:22:46 +0000

    Passes test for entering player names and displaying it

--- spec/features/form_name_spec.rb
+++ spec/features/form_name_spec.rb
@@ -1,7 +1,20 @@
 feature 'Name form' do
   scenario 'expects players to fill in their names in a form' do
     visit('/')
+    fill_in(:player_1_name, with: 'Dave')
+    fill_in(:player_2_name, with: 'Mittens')
+    click_button 'Submit'
+    expect(page).to have_content "Dave vs. Mittens"
     #finding an input tab called name in the form and checks if it has a value
-    find_field('name').value
+    # find_field('name').value
   end
+
+  # scenario 'expects players to fill in their names in a form' do
+  #   visit('/')
+  #   fill_in('name', with: 'Viking')
+  #   visit('/names')
+  #   expect(page).to have_content('Viking')
+
+  # end
+
 end
